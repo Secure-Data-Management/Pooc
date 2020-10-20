2ca4a45d513b80c67eebee1d6ba398359dace03f
s2471264 <a.a.l.destalenx@student.utwente.nl>
2020-10-20 18:45:16 +0200

    mEnc and mDec, to be tested with working func e

--- mpeck.py
+++ mpeck.py
@@ -19,21 +19,22 @@ def hash_G2_to_M(g, length=64):
 
 def mPECK(pk_list, W, params, message=""):
     """
-    multi Public key Encryption with Conjuctive Keyword
-    Performs the encryption of the keywords, using the mPECK model. Encrypts the keywords in W using the public keys in pk_list
+    multi Public key Encryption with Conjuctive Keyword. Encrypts both message and keywords !
+    Performs the encryption of the keywords and of the message, using the mPECK model. Encrypts the keywords in W using the public keys in pk_list
     Parameters:
         pk: list of keys (from Keygen.keys)
         W: set of keywords
         params: a dictionary containing the security parameters
     Returns:
-        [E, S] as in the paper, E=b"" if there was no message
-            S = [A, B, C] with A=g^r, B=[B1, ..., Bn] and C=[C1, ..., Cl]
+        [E, A, B, C] as in the paper, E=b"" if there was no message
+            [A, B, C] with A=g^r, B=[B1, ..., Bn] and C=[C1, ..., Cl]
     """
+    #TODO: to remove if hashes accept strings
     # convert keywords to bytes:
-    for i, kw in enumerate(W):
-        if isinstance(kw, str):
-            kw_bytes = kw.encode("utf-8")
-            W[i] = kw_bytes
+    # for i, kw in enumerate(W):
+        # if isinstance(kw, str):
+        #     kw_bytes = kw.encode("utf-8")
+        #     W[i] = kw_bytes
 
     # selects two random values in Zp*
     s = GT.order().random()
@@ -56,25 +57,43 @@ def mPECK(pk_list, W, params, message=""):
 
     #encode the message
     E = b""
-    # if len(message) > 0:
-    #     if not isinstance(message, bytes):
-    #         message = message.encode("utf-8")
-    #     E = hash_G2_to_M(params["e"](params["g"], params["g"]) ** (r*s))
-    #     print(E)
+    if len(message) > 0:
+        #TODO: to remove if hashes accept strings
+        # if not isinstance(message, bytes):
+        #     message = message.encode("utf-8")
+        E = hash_G2_to_M(params["e"](params["g"], params["g"]) ** (r*s)) ^ message
+        print(E)
     #FIXME: e n'est pas de la bonne forme, elle ne peut pas prendre (g,g) comme argument donc impossible de faire le chiffrement du message
 
-    S = [A, B, C]
-    return [E, S]
+    return [E, A, B, C]
 
 
 
-
+def mDEC(xj, E, Bj, A):
+    """Decrypts the cipher E, using private key xj, Bj and A"""
+    Xj = hash_G2_to_M(params["e"](A, Bj)**(1/xj))
+    m = E ^Xj
+    m = m.decode()
+    return m
 
 
 
 if __name__ == "__main__":
-    k = KeyGen(3)
+    # number of participants
+    n = 3
+    k = KeyGen(n)
     params = {"G1":G1, "G2":G2, "e":k.e, "H1":k.h1, "H2":k.h2, "g":k.g1}
     pk_list = [key[1] for key in k.keys]
     sk_list = [key[0] for key in k.keys]
-    mPECK(pk_list, ["test", "encryption"], params, message='This is the message')
+    #encryption of the message
+    message = "This is the message"
+    keywords = ["test", "encryption"]
+    # there are 3 clients, only allow 0 and 1 to search and decrypt
+    recipients = [0, 1]
+    recipients_pk = [pk_list[r] for r in recipients]
+    E, A, B, C = mPECK(recipients_pk, keywords, params, message=message)
+    print(f"Message \"{message}\" encrypted, only recipients {recipients} are allowed to decrypt")
+    # decrypt as 0
+    for i in range(n):
+        m = mDEC(sk_list[i], E, B[i], A)
+        print(f"Client {i}: decryption is: {m}")
