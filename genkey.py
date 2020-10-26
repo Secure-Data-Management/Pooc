21af3912558891cbf43c2a5fa5d421ad88795c42
Erwan Kessler <45232488+erwan-kessler@users.noreply.github.com>
2020-10-27 00:52:17 +0100

    Fixed it, will push lib tomorrow

--- genkey.py
+++ genkey.py
@@ -14,6 +14,8 @@ import hashlib
 
 from crypto.bn256 import *
 
+from pypbc import *
+
 
 class KeyGen:
     """ we are using RELIC as the underlying curve gen ( BLS-381 ) we will implement full parameter through charm like later
@@ -21,30 +23,55 @@ class KeyGen:
 
     def __init__(self, n: int):
         # those are the hash function which hash to a G1Element using sha3 256 and 512
-        self.h1: Callable[[Union[bytes, bytearray, str]], CurvePoint] = self.get_hash_function(hashlib.sha3_256)
-        self.h2: Callable[[Union[bytes, bytearray, str]], CurvePoint] = self.get_hash_function(hashlib.sha3_512)
+
         # this is the element e (not sure tho)
-        self.e: Callable[[CurveTwist, CurvePoint], gfp_12] = lambda e1, e2: optimal_ate(e1, e2)
-        self.pub_keys: List[CurvePoint] = []
-        self.keys: List[Tuple[int, CurvePoint]] = []
-        self.priv_keys: List[int] = []
-        self.g:CurvePoint = curve_G
+        self.params: Parameters = Parameters(qbits=512, rbits=160)
+        self.pairing: Pairing = Pairing(self.params)
+        self.h1: Callable[[Union[bytes, bytearray, str]], Element] = self.get_hash_function(self.pairing, hashlib.sha3_256)
+        self.h2: Callable[[Union[bytes, bytearray, str]], Element] = self.get_hash_function(self.pairing, hashlib.sha3_512)
+        self.e: Callable[[Element, Element], Element] = lambda e1, e2: self.pairing.apply(e1, e2)
+        self.pub_keys: List[Element] = []
+        self.keys: List[Tuple[Element, Element]] = []
+        self.priv_keys: List[Element] = []
+        self.g = Element.random(self.pairing, G2)
         for _ in range(n):
-            sk = rand_elem()
-            pk = self.g.scalar_mul(sk)
+            sk:Element = Element.random(self.pairing, Zr)
+            pk:Element = Element(self.pairing, G2, value=self.g ** sk)
             self.keys.append((sk, pk))
             self.pub_keys.append(pk)
             self.priv_keys.append(sk)
-            print(pk,type(pk))
-    def get_hash_function(self, hash_function: Callable[[Union[bytes, bytearray]], Any]) -> Callable[[Union[bytes, bytearray, str]], CurvePoint]:
-        return lambda text: g1_hash_to_point(hash_function(text).digest()) if isinstance(text, (bytes, bytearray)) else g1_hash_to_point(hash_function(text.encode()).digest())
+
+    def get_hash_function(self, pairing, hash_function: Callable[[Union[bytes, bytearray]], Any]) -> Callable[[Union[bytes, bytearray, str]], Element]:
+        return lambda text: Element.from_hash(pairing, G1, hash_function(text).digest()) if isinstance(text, (bytes, bytearray)) else Element.from_hash(pairing, G1,
+                                                                                                                                                        hash_function(text.encode()).digest())
 
     @staticmethod
     def test():
-        priv, pub = g2_random()
-        m = b"Some message"
-        signature = g1_hash_to_point(m).scalar_mul(priv)
-        assert optimal_ate(pub, g1_hash_to_point(m)) == optimal_ate(twist_G, signature)
+        # this is a test for the BLS short signature system
+        params = Parameters(qbits=512, rbits=160)
+        pairing = Pairing(params)
+
+        # build the common parameter g
+        g = Element.random(pairing, G2)
+
+        # build the public and private keys
+        private_key = Element.random(pairing, Zr)
+        public_key = Element(pairing, G2, value=g ** private_key)
+
+        # set the magic hash value
+        hash_value = Element.from_hash(pairing, G1, "hashofmessage")
+
+        # create the signature
+        signature = hash_value ** private_key
+
+        # fill temp2
+        temp2 = pairing.apply(hash_value, public_key)
+
+        # and again...
+        temp1 = pairing.apply(signature, g)
+
+        # compare
+        assert (temp1 == temp2)
         print("test passed")
 
     def __str__(self):
--- mpeck.py
+++ mpeck.py
@@ -2,87 +2,84 @@
 from typing import Callable, List, Tuple
 
 from crypto.bn256 import *
-from genkey import KeyGen
+from genkey import *
 # Antoine
 import hashlib
 
-xor: Callable[[bytes, bytes], bytearray] = lambda part1, part2: bytearray([e1 ^ e2 for (e1, e2) in zip(part1, part2)])
+
+def xor(message: Union[bytearray, bytes, str], key: Union[bytearray, bytes]) -> bytearray:
+    """Takes a string message and a bytes sequence and hash it with a displacing key sequence"""
+    res: bytearray = bytearray()
+    if type(message) == str:
+        message = bytearray(message.encode())
+    elif type(message) == bytes:
+        message = bytearray(message)
+    if type(key) == bytes:
+        key = bytearray(key)
+    if len(message) > len(key):
+        for i in range(len(message)):
+            res.append(message[i] ^ key[i % len(key)])
+    else:
+        for i in range(len(message)):
+            res.append(key[i] ^ message[i % len(message)])
+    return res
 
 
-def hash_G2_to_M(g: str, length=64) -> bytes:
-    """Takes an element of G2, and hashes it to a bytes object"""
-    g_bytes: bytes = g.encode()
-    g_hash: bytes = hashlib.sha3_512(g_bytes).digest()
-    result: bytes = g_hash
-    # TODO: find a way to generate a hash of length equal to the given length
-    while len(result) < length:
-        result += g_hash
-    result: bytes = result[:length]
-    return result
-
-
-def mpeck(pk_list:List[CurvePoint], W:List[str], genkey: KeyGen, message:str="")-> Tuple[bytearray,CurvePoint, List[CurvePoint], List[CurvePoint]]:
+def mpeck(pk_list: List[Element], keyword_list: List[str], genkey: KeyGen, message: str = "") -> Tuple[bytearray, Element, List[Element], List[Element]]:
     """
     multi Public key Encryption with Conjuctive Keyword. Encrypts both message and keywords !
     Performs the encryption of the keywords and of the message, using the mPECK model. Encrypts the keywords in W using the public keys in pk_list
     Parameters:
         pk: list of keys (from Keygen.keys)
-        W: set of keywords
+        keyword_list: set of keywords
         params: a dictionary containing the security parameters
     Returns:
         [E, A, B, C] as in the paper, E=b"" if there was no message
             [A, B, C] with A=g^r, B=[B1, ..., Bn] and C=[C1, ..., Cl]
+            :param keyword_list:
+            :param pk_list:
+            :param message:
+            :param genkey:
     """
-    # TODO: to remove if hashes accept strings
-    # convert keywords to bytes:
-    # for i, kw in enumerate(W):
-    # if isinstance(kw, str):
-    #     kw_bytes = kw.encode("utf-8")
-    #     W[i] = kw_bytes
-
     # selects two random values in Zp*
-    s: int = rand_elem()
-    r: int = rand_elem()
+    s: Element = Element.random(genkey.pairing, Zr)
+    r: Element = Element.random(genkey.pairing, Zr)
     # computes the A=g^r
-    A:CurvePoint = g1_scalar_base_mult(r)
+    A: Element = genkey.g ** r
     # computes B = pk**s for each public key
     n = len(pk_list)
-    B :List[CurvePoint]= []
+    B: List[Element] = []
     for j in range(n):
-        yj: CurvePoint = genkey.pub_keys[j]
-        B.append(yj.scalar_mul(s))
+        yj: Element = genkey.pub_keys[j]
+        B.append(yj ** s)
     # computes C = (h^r)(f^s) for each keyword
-    l = len(W)
-    C: List[CurvePoint]= []
-    for i, kw in enumerate(W):
+    l = len(keyword_list)
+    C: List[Element] = []
+    for i, kw in enumerate(keyword_list):
         h = genkey.h1(kw)
         f = genkey.h2(kw)
         # ** -> * ; * -> +
-        temp1: CurvePoint = h.scalar_mul(r)
-        temp2: CurvePoint = f.scalar_mul(s)
-        C.append(temp1.add(temp2))
+        temp1: Element = h ** r
+        temp2: Element = f ** s
+        C.append(temp1 * temp2)
     # encode the message
     E: bytearray = bytearray()
     if len(message) > 0:
-        # TODO: to remove if hashes accept strings
-        # if not isinstance(message, bytes):
-        #     message = message.encode("utf-8")
-        e_g_g: gfp_12 = genkey.e(twist_G, genkey.g)
-        e_r_s = r * s
-        e_g_g1: gfp_12 = e_g_g.exp(e_r_s)
-        e_g_g2: bytes = hash_G2_to_M(e_g_g1.__repr__())
-        E: bytearray = xor(e_g_g2, message.encode())
+        e_g_g: Element = genkey.pairing.apply(genkey.g, genkey.g)
+        e_r_s: Element = r * s
+        e_g_g1: Element = e_g_g ** e_r_s
+        e_g_g2: bytes = hashlib.sha256(e_g_g1.__str__().encode()).digest()
+        E: bytearray = xor(message.encode(), e_g_g2)
         print(E)
-    # FIXME: e n'est pas de la bonne forme, elle ne peut pas prendre (g,g) comme argument donc impossible de faire le chiffrement du message
 
     return E, A, B, C
 
 
-def mdec(xj: int, E: bytearray, Bj:CurvePoint, A:CurvePoint, k:KeyGen):
+def mdec(xj: Element, E: bytearray, Bj: Element, A: Element, k: KeyGen):
     """Decrypts the cipher E, using private key xj, Bj and A"""
-    print(A.x,A.y,A.z)
-    e_A_Bj: gfp_12 = k.e(A,Bj)
-    Xj = hash_G2_to_M(e_A_Bj.mul_scalar(1 / xj))
+    e_A_Bj: Element = k.e(A, Bj)
+    res: Element = e_A_Bj ** (~xj)
+    Xj = hashlib.sha256(res.__str__().encode()).digest()
     m = xor(E, Xj)
     m = m.decode()
     return m
@@ -90,17 +87,17 @@ def mdec(xj: int, E: bytearray, Bj:CurvePoint, A:CurvePoint, k:KeyGen):
 
 if __name__ == "__main__":
     # number of participants
-    n = 3
-    k = KeyGen(n)
+    _n = 3
+    k = KeyGen(_n)
     # encryption of the message
-    message = "This is the message"
-    keywords = ["test", "encryption"]
+    _message = "This is the message"
+    _keywords = ["test", "encryption"]
     # there are 3 clients, only allow 0 and 1 to search and decrypt
-    recipients = [0, 1]
-    recipients_pk = [k.pub_keys[r] for r in recipients]
-    E, A, B, C = mpeck(recipients_pk, keywords, k, message=message)
-    print(f"Message \"{message}\" encrypted, only recipients {recipients} are allowed to decrypt")
+    _recipients = [0, 1]
+    _recipients_pk = [k.pub_keys[r] for r in _recipients]
+    _E, _A, _B, _C = mpeck(_recipients_pk, _keywords, k, message=_message)
+    print(f"Message \"{_message}\" encrypted, only recipients {_recipients} are allowed to decrypt")
     # decrypt as 0
-    for i in range(n):
-        m = mdec(k.priv_keys[i], E, B[i], A, k)
-        print(f"Client {i}: decryption is: {m}")
+    for _i in range(len(_recipients)):
+        m = mdec(k.priv_keys[_i], _E, _B[_i], _A, k)
+        print(f"Client {_i}: decryption is: {m}")
